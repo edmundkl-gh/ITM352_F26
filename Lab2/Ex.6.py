5e7c18af512ad3a685b5c5e89ad31680289b0cde
edmundkl-gh <edmundkl@hawaii.edu>
2026-09-04 12:21:07 -1000

    Update Ex.6.py

---
+++ Lab2/Ex.6.py
@@ -0,0 +1,8 @@
+#ask the user to enter their weight in pounds.
+#returns the weight in kilograms
+
+# Name: Edmund Liu
+# Date: Sept 4, 2026
+
+user_weight = float(input("enter a weight in pounds: ")) * 0.453592
+print("your weight in kilograms is:", user_weight, "kilograms")
