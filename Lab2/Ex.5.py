00d676b26a553a5a7b0f048b721fe88709c9b6ad
edmundkl-gh <edmundkl@hawaii.edu>
2026-09-04 12:13:45 -1000

    new examples

---
+++ Lab2/Ex.5.py
@@ -0,0 +1,11 @@
+#ask the user to make a sentence of their choice.
+#return the number of characters that were in the string entered
+
+# Name: Edmund Liu
+# Date: Sept 4, 2026
+
+user_input = input("please type a sentence of your choosing: ")
+number_of_characters = len(user_input)
+
+print("you entered:", user_input)
+print("the number of characters in your sentence is:", number_of_characters)
